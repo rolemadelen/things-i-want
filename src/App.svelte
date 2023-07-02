0657069efb3f1fd8d5d9ea17d10cc40eddfc9487
Jii Eu <hello@jiieu.com>
2023-07-02 10:38:57 -0400

    connected supabase

--- src/App.svelte
+++ src/App.svelte
@@ -1,83 +1,24 @@
 <script>
+  import { supabase } from '/src/supabaseClient';
   import { onMount } from 'svelte';
 
-  const numberOfitems = 8;
-  let content;
-  let lastScrollTop;
-  let offsetY;
-  let offset = 0;
+  let itemList = [];
 
-  $: {
-    console.log(`lastScrollTop: ${lastScrollTop}`)
-    console.log(`${offset}    ${Math.floor(offset)}    ${Math.ceil(offset)}`);
-  }
   onMount(() => {
-    offsetY = document.body.offsetHeight;
+    fetchItems().then(res => itemList = Array.from(res));
   })
 
-  const items = [
-    {
-      title: 'Azio Cascade Slim',
-      img: '/src/assets/azio-cascade-slim.png'
-    },
-    {
-      title: 'Keychron Q8',
-      img: '/src/assets/keychron-q8.png'
-    },
-    {
-      title: 'Kinesis Advantage 2',
-      img: '/src/assets/kinesis-advantage-2.png'
-    },
-    {
-      title: 'Ergodox EZ',
-      img: '/src/assets/ergodox-ez.png'
-    },
-    {
-      title: 'Nagado Daiko',
-      img: '/src/assets/nagado-daiko.png'
-    },
-    {
-      title: 'ROLAND (Taiko-1)',
-      img: '/src/assets/roland-taiko-1.png'
-    },
-    {
-      title: 'Rayneo XR Glasses',
-      img: '/src/assets/rayneo-xr-glasses.png'
-    },
-    {
-      title: 'Apple Pro Display XDR',
-      img: '/src/assets/apple-pro-display-xdr.png'
+  async function fetchItems() {
+    const { data, error } = await supabase
+      .from('Item')
+      .select('id, title, img_src, bg_color, cost')
+      .order('title');
 
-    },
-    {
-      title: 'Atmoph Window 2',
-      img: '/src/assets/atmoph-window-2.png'
-    },
-  ];
-
-  const handleScroll = () => {
-    offset = numberOfitems - (lastScrollTop / offsetY);
-    const main = document.querySelector('.main');
-
-    if(offset > 7.5) {
-      main.style.backgroundColor = '#5ca288';
-    } else if (offset > 6.5) {
-      main.style.backgroundColor = '#819cb5';
-    } else if (offset > 5.5) {
-      main.style.backgroundColor = '#75bdeb';
-   } else if (offset > 4.5) {
-      main.style.backgroundColor = '#fff';
-    } else if (offset > 3.5) {
-      main.style.backgroundColor = '#b64b319e';
-    } else if (offset > 2.5) {
-      main.style.backgroundColor = '#b865ff9e';
-    } else if (offset > 1.5) {
-      main.style.backgroundColor = '#fff';
+    if(error) {
+      console.error('error')
     } else {
-      main.style.backgroundColor = '#fff';
+      return data;
     }
-
-    lastScrollTop = content.scrollTop;
   }
 
 </script>
@@ -86,10 +27,11 @@
   <header>
     <div class='logo'>Things I Want</div>
   </header>
-  <div class='main' bind:this={content} on:scroll={handleScroll}>
-    {#each items as item, i}
-      <div class='fullscreen item' id={i+1}>
-        <img src="{item.img}"/>
+  <div class='main'>
+    {#each itemList as item}
+      <div class='fullscreen item' id={item.id}>
+        <img src="{item.img_src}"/>
+        <div class='cost'>${item.cost}</div>
       </div>
     {/each}
   </div>
@@ -104,8 +46,8 @@
   </footer>
   <nav>
     <ul>
-      {#each items as item, i}
-        <a data-id={i+1} href="#{i+1}"><li>{item.title}</li></a>
+      {#each itemList as item}
+        <a data-id={item.id} href="#{item.id}"><li>{item.title}</li></a>
       {/each}
     </ul>
   </nav>
@@ -113,7 +55,6 @@
 
 <style>
   .main {
-    background-color: #5ca288;
     z-index: -1;
     height: 100vh;
     overflow: auto;
@@ -144,7 +85,7 @@
     position: fixed;
     bottom: 20px;
     left: 0;
-    width: 320px;
+    width: clamp(200px, 40vw, 320px);
     margin-left: 15px;
     line-height: 1.35;
   }
@@ -186,15 +127,21 @@
   }
 
   img {
-    width: 50%;
-    min-width: 400px;
+    width: 100%;
   }
 
   .fullscreen {
+    min-width: 400px;
+    max-width: 35vw;
+    margin: 0 auto;
     height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
+  }
+
+  .cost {
   }
 
   a {
